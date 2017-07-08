da79fc36d6092662bbe33aed019c0cb0786202b2
Siburg <jelle.sjoerdsma@gmail.com>
2017-07-09 11:40:59 +1200

    fixed small error

--- minesweep.py
+++ minesweep.py
@@ -1,8 +1,26 @@
-#
+"""
+A minesweeper game without a GUI.
+"""
+
+def get_game_parameters():
+    pass
+
+
+def setup_game():
+    pass
+
+
+def display_board():
+    pass
+
+
+def get_move():
+    pass
 
 
 def main():
     pass
 
 
-if __name__ == '__main__': run main()
+if __name__ == '__main__':
+    main()
