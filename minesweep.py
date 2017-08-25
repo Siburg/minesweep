33e63982b1b35b7d59f702b053b7aa74824e0d87
Siburg <jelle.sjoerdsma@gmail.com>
2017-08-25 19:36:59 +1200

    Started to add the display_board function; not yet with any test cases

--- minesweep.py
+++ minesweep.py
@@ -11,6 +11,7 @@ def get_game_parameters(max_width=76, max_height=20):
     MIN_WIDTH = 3
     MIN_HEIGHT = 3
     MIN_MINES = 1
+
     def get_single_input(prompt, min_value, max_value):
         """Helper function to get each input one at a time."""
         while True:
@@ -37,7 +38,7 @@ def get_game_parameters(max_width=76, max_height=20):
 def setup_game(width, height, number_of_mines):
     """Set up the 2-dimensional lists with mines and with the initial board to display"""
     # First, create 1-dimensional list of random mines
-    random_mines = sample(range(width*height), number_of_mines)
+    random_mines = sample(range(width * height), number_of_mines)
     # translate the 1-dimensional list into a list with x,y coordinates for each mine
     mines = []
     for mine in random_mines:
@@ -50,9 +51,19 @@ def setup_game(width, height, number_of_mines):
     return mines, board
 
 
-def display_board():
-    print('hello')
-    pass
+def display_board(board):
+    print('  |',end="")
+    for x in range(1, len(board) + 1):
+        if x % 10 == 0 and x > 0:
+            print(x // 10, end="")
+        else:
+            print(' ', end="")
+    print('|')
+
+    print(' x|', end="")
+    for x in range(1, len(board) + 1):
+        print(x % 10, end="")
+    print('|')
 
 
 def get_move():
@@ -62,8 +73,7 @@ def get_move():
 def main():
     width, height, number_of_mines = get_game_parameters()
     mines, board = setup_game(width, height, number_of_mines)
-
-
+    display_board(board)
 
 if __name__ == '__main__':
     main()
