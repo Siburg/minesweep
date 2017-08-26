a164c0bfefe71a8e1b103e3d572e1e6488d8771c
Siburg <jelle.sjoerdsma@gmail.com>
2017-08-26 16:53:23 +1200

    Slightly revamped display board to put coordinate printing in small sub-functions.

--- minesweep.py
+++ minesweep.py
@@ -37,37 +37,45 @@ def get_game_parameters(max_width=76, max_height=20):
 
 
 def setup_game(width, height, number_of_mines):
-    """Set up the 2-dimensional lists with mines and with the initial board to display"""
+    """Set up the 2-dimensional lists with mines and with the initial board to display.
+    Output is mines in list of tuples with coordinates (x,y), and
+    board in form 2-dimensional list form, with y as dimension 1 and x as dimension 2"""
     # First, create 1-dimensional list of random mines
     random_mines = sample(range(width * height), number_of_mines)
     # translate the 1-dimensional list into a list with x,y coordinates for each mine
     mines = []
     for mine in random_mines:
         mines.append((mine % width, mine // width))
+
     board = []
     for y in range(height):
         board.append([])
         for x in range(width):
             board[y].append('~')
+
     return mines, board
 
 
 def display_board(board):
     """Print the board with x and y axis in human counting form."""
+    def print_x_coordinate_singles(widthp1):
+        print('  x|', end="")
+        for xp1 in range(1, widthp1):
+            print(xp1 % 10, end="")
+        print('|')
+
+    def print_x_coordinate_tens(widthp1):
+        print('   |', end="")
+        for xp1 in range(1, widthp1):
+            if xp1 % 10 == 0:
+                print(xp1 // 10, end="")
+            else:
+                print(' ', end="")
+        print('|')
+
     os.system('clear')
-    print('   |', end="")
-    for xp1 in range(1, len(board[0]) + 1):
-        if xp1 % 10 == 0:
-            print(xp1 // 10, end="")
-        else:
-            print(' ', end="")
-    print('|')
-
-    print('  x|', end="")
-    for xp1 in range(1, len(board[0]) + 1):
-        print(xp1 % 10, end="")
-    print('|')
-
+    print_x_coordinate_tens(len(board[0]) + 1)
+    print_x_coordinate_singles(len(board[0]) + 1)
     print('-' * (len(board[0])+7))
 
     for y in range(len(board) - 1, -1, -1):
@@ -77,18 +85,8 @@ def display_board(board):
         print('|{0:2}y'.format(y + 1))
 
     print('-' * (len(board[0])+7))
-    print('  x|', end="")
-    for xp1 in range(1, len(board[0]) + 1):
-        print(xp1 % 10, end="")
-    print('|')
-
-    print('   |', end="")
-    for xp1 in range(1, len(board[0]) + 1):
-        if xp1 % 10 == 0:
-            print(xp1 // 10, end="")
-        else:
-            print(' ', end="")
-    print('|')
+    print_x_coordinate_singles(len(board[0]) + 1)
+    print_x_coordinate_tens(len(board[0]) + 1)
 
 def get_move():
     pass
