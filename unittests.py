fedb95c0b56f7a3bfc7a1f324e8f74039da49643
Siburg <jelle.sjoerdsma@gmail.com>
2017-08-30 11:52:42 +1200

    Restarted work on unit tests; starting with separate tests for the setup_mines and the setup_board functions.

--- unittests.py
+++ unittests.py
@@ -2,77 +2,19 @@
 
 import minesweep
 import unittest
-from unittest import mock
-import sys
-import io
+#from unittest import mock
+#import sys
+#import io
 
 
-class TestGetGameParameters(unittest.TestCase):
-    """ test function get_game_parameters"""
-
-    def setUp(self):
-        self.print_redirect = io.StringIO()
-        sys.stdout = self.print_redirect
-
-    def tearDown(self):
-        self.print_redirect.__del__()
-        sys.stdout = sys.__stdout__
-
-    def test_number_of_return_values(self):
-        """ check that we get 3 return values for normal input"""
-        with mock.patch('builtins.input', side_effect=('9', '9', '9')):
-            values = minesweep.get_game_parameters()
-        self.assertEqual(len(values), 3)
-
-    def test_all_return_values_are_integer(self):
-        """ check that all of the return values are integer for normal input"""
-        # you could split this into separate tests, but that seems like overkill
-        with mock.patch('builtins.input', side_effect=('9', '9', '9')):
-            values = minesweep.get_game_parameters()
-        self.assertTrue(type(values[0]) is int
-                        and type(values[1]) is int and type(values[2]) is int)
-
-#    """fix the rest below
-#    def test_max_width_compliance(self):
-#        """check that returned width value remains within specified max_width"""
-#        max_width = 1
-#        values = minesweep.get_game_parameters(max_width=max_width)
-#        self.assertLessEqual(values[0], max_width)
-
-#    def test_max_height_compliance(self):
-#        """check that returned height value remains within specified max_height"""
-#        max_height = 1
-#        values = minesweep.get_game_parameters(max_height=max_height)
-#        self.assertLessEqual(values[1], max_height)
-
-#    def test_minimum_values(self):
-#        """check that all returned values are at least 1"""
-#        # this assumes that you also need at least 1 mine
-#        # this could be split into separate tests, but that seems like overkill
-#        values = minesweep.get_game_parameters()
-#        self.assertTrue(values[0] >= 1 and values[1] >= 1 and values[2] >= 1)
-
-#    def test_number_of_mines_can_fit(self):
-#        """check that number of mines is less than the area of the board"""
-#        # also allowing for at least 1 blank space; instead of having 100% mines
-#        values = minesweep.get_game_parameters()
-#        self.assertLess(values[2], values[0] * values[1])
-
-
-@unittest.skip('Need to refactor for split')
-class TestSetupGame(unittest.TestCase):
-    """ test function setup_game"""
-
-    """The setup_game function has been split into two separate funtions.
-    Need to separate tests for that."""
+class TestSetupMines(unittest.TestCase):
+    """ test function setup_mines"""
 
     def setUp(self):
         self.width = 20
         self.height = 10
         self.number_of_mines = 30
-        self.results = minesweep.setup_game(self.width, self.height, self.number_of_mines)
-        self.mines = self.results[0]
-        self.board = self.results[1]
+        self.mines = minesweep.setup_mines(self.width, self.height, self.number_of_mines)
 
     def test_returned_mines_is_list(self):
         """check that mines are returned in the form of a list"""
@@ -102,6 +44,16 @@ class TestSetupGame(unittest.TestCase):
         # note that coordinates start with 0, instead of 1
         self.assertTrue(all(mine[1] < self.height for mine in self.mines))
 
+
+class TestSetupBoard(unittest.TestCase):
+    """ test function setup_board"""
+
+    def setUp(self):
+        self.width = 20
+        self.height = 10
+        self.board = minesweep.setup_board(self.width, self.height)
+
+
     def test_returned_board_is_list(self):
         """check that board is returned in the form of a list"""
         self.assertEqual(type(self.board), list)
@@ -118,47 +70,6 @@ class TestSetupGame(unittest.TestCase):
         """check that number of items in the first sublist, representing x coordinate, equals width"""
         # note that checking for later sublists should be redundant
         self.assertEqual(len(self.board[0]), self.width)
-
-
-@unittest.skip('No tests for display_board yet')
-class TestDisplayBoard(unittest.TestCase):
-    """ test function display_board"""
-    # note this is mostly still a bunch of nonsense tests to understand print redirection
-    # does not yet have anything to do with the program itself
-
-    def setUp(self):
-        self.print_redirect = io.StringIO()
-        sys.stdout = self.print_redirect
-
-    def test_something_got_printed(self):
-        minesweep.display_board()
-        printed = self.print_redirect.getvalue()
-        # commented out statements below cause an exit failure
-        # sys.stdout = sys.__stdout__
-        # print_redirect.close()
-        self.assertEqual(printed, 'hello\n')
-
-    def test_same_thing_again_got_printed(self):
-        print('hello again')
-        printed = self.print_redirect.getvalue()
-        # commented out statements below cause an exit failure
-        # sys.stdout = sys.__stdout__
-        # print_redirect.close()
-        self.assertEqual(printed, 'hello again\n')
-
-    def test_print_redirect_got_closed(self):
-        self.assertFalse(self.print_redirect.closed)
-
-    def test_what_happens_now(self):
-        printed = self.print_redirect.readline()
-        self.assertEqual(printed, "")
-
-    def tearDown(self):
-        # sys.stdout = sys.__stdout__
-        self.print_redirect.__del__()
-        sys.stdout = sys.__stdout__
-        # io.StringIO.close()
-        pass
 
 
 if __name__ == '__main__':
